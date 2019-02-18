05bb4dca03c1a14bc49e3c66c0eecc62828a16dd
浅忆梦微凉 <3538922590@qq.com>
2019-02-18 19:33:38 +0800

    不懂

---
+++ 文件压缩/HuffmanTree.cpp
@@ -0,0 +1,40 @@
+#pragma once
+#include"FileComepressHuffM.h"
+#include<vector>
+#include<deque>
+
+template<class W>
+struct HuffmanTreeNode{
+	HuffmanTreeNode(const W& weight)
+	:_weight(weight)
+	, _pleft(nullptr)
+	, _pright(nullptr){}
+
+	HuffmanTreeNode<W>* _pleft;
+	HuffmanTreeNode<W>* _pright;
+
+	W _weight;
+
+};
+
+//·Âº¯Êý£º
+
+
+template<class W>
+class HuffmanTree{
+	typedef HuffmanTreeNode<W> Node;
+	typedef Node* pNode;
+public:
+	HuffmanTree()
+		:_pRoot(nullptr){}
+	void CreateHuffmanTree(const std::vector<W>& v){
+		if (v.empty()){
+			return;
+		}
+		std::priority_queue<pNode> q;
+		for (size_t i = 0; i < v.size(); ++i){
+			q.push(new Node(v[i]));
+		}
+	}
+	pNode _pRoot;
+};
---
+++ 文件压缩/test.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"FileComepressHuffM.h"
+
+
+void FileComepressHuffM::CompressFile(const string& strFilePath){
+	FILE* fIn = fopen(strFilePath.c_str(), "r");
+	if (nullptr == fIn){
+		cout << "文件打开失败" << endl;
+		return;
+	}
+	char* pReadBuff = new char[1024];
+	while (1){
+		size_t rdSize = fread(pReadBuff, 1, 1024, fIn);//返回值rd为实际读到的字节数
+		if (rdSize == 0){
+			break;
+		}
+		for (size_t i = 0; i < rdSize; ++i){
+			;
+		}
+	}
+
+}
